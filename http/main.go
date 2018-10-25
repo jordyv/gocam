e0d45993568a1fac92608dd9f5c8d46f9e3cfd1f
Jordy Versmissen <postvoorjordy@gmail.com>
2018-10-25 21:07:54 +0200

    Group alerts in web UI by date

--- http/main.go
+++ http/main.go
@@ -2,6 +2,7 @@ package gocamhttp
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -29,6 +30,35 @@ type alertApiItem struct {
 	ImageName string    `json:"image_name"`
 }
 
+func getAllItems(imagePath string) ([]alertApiItem, error) {
+	imageFiles, err := ioutil.ReadDir(fmt.Sprintf("%s/alert", imagePath))
+	if err != nil {
+		log.Errorln("error reading image directory:", err)
+		return nil, errors.New("could not read images in directory")
+	}
+	files := make([]alertApiItem, 0)
+	for _, image := range imageFiles {
+		if !image.IsDir() {
+			files = append(files, alertApiItem{image.ModTime(), image.Name()})
+		}
+	}
+	return files, nil
+}
+
+func getAllGroupedItems(imagePath string) (map[time.Time]([]alertApiItem), error) {
+	items, err := getAllItems(imagePath)
+	if err != nil {
+		return nil, err
+	}
+	m := make(map[time.Time]([]alertApiItem), 0)
+	for _, i := range items {
+		groupTimestamp := time.Date(i.Timestamp.Year(), i.Timestamp.Month(), i.Timestamp.Day(), int(0), int(0), int(0), int(0), time.Local)
+		m[groupTimestamp] = append(m[groupTimestamp], i)
+	}
+
+	return m, nil
+}
+
 func (h *GocamHttp) Listen() {
 	box := packr.NewBox("./webui")
 
@@ -36,22 +66,13 @@ func (h *GocamHttp) Listen() {
 	handler.Handle("/", http.FileServer(box))
 	handler.Handle("/images/", http.StripPrefix("/images", http.FileServer(http.Dir(h.Options.config.ImagePath))))
 	handler.HandleFunc("/api/alerts", func(w http.ResponseWriter, r *http.Request) {
-		imageFiles, err := ioutil.ReadDir(fmt.Sprintf("%s/alert", h.Options.config.ImagePath))
-		if err != nil {
-			log.Errorln("error reading image directory:", err)
-			return
-		}
-		files := make([]alertApiItem, 0)
-		for _, image := range imageFiles {
-			if !image.IsDir() {
-				files = append(files, alertApiItem{image.ModTime(), image.Name()})
-			}
-		}
-		json, err := json.Marshal(files)
-		if err != nil {
-			log.Errorln("error while serializing to JSON", err)
-			return
-		}
+		files, _ := getAllItems(h.Options.config.ImagePath)
+		json, _ := json.Marshal(files)
+		fmt.Fprint(w, string(json))
+	})
+	handler.HandleFunc("/api/alerts/grouped", func(w http.ResponseWriter, r *http.Request) {
+		files, _ := getAllGroupedItems(h.Options.config.ImagePath)
+		json, _ := json.Marshal(files)
 		fmt.Fprint(w, string(json))
 	})
 	log.Fatalln(http.ListenAndServe(h.Options.config.HTTPAddr, handler))
--- http/webui/app/main.js
+++ http/webui/app/main.js
@@ -1,21 +1,38 @@
 var data = {
     currentDate: new Date(),
-    alerts: {}
+    alertGroups: {},
+    totalAlerts: 0,
+}
+
+function sortAlertGroups(groups) {
+    var sortedGroupKeys = Object.keys(groups).sort((a, b) => new Date(b).getTime() - new Date(a).getTime())
+    var sortedGroups = {}
+    sortedGroupKeys.forEach(g => sortedGroups[g] = sortAlerts(groups[g]))
+    return sortedGroups
 }
 
 function sortAlerts(alerts) {
     return alerts.sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime())
 }
 
-fetch('/api/alerts')
+function getItemCount(groups) {
+    var count = 0
+    Object.keys(groups).forEach(g => count += groups[g].length)
+    return count
+}
+
+fetch('/api/alerts/grouped')
     .then(r => r.json())
-    .then(d => data.alerts = sortAlerts(d))
+    .then(d => {
+        data.alertGroups = sortAlertGroups(d)
+        data.totalAlerts = getItemCount(d)
+    })
 
 var app = new Vue({
     el: '#app',
     data: data,
     methods: {
-        formatDate: function (dateString) {
+        formatDateTime: function (dateString) {
             var date = new Date(dateString)
             var day = date.getDate(),
                 month = date.getMonth() + 1,
@@ -29,6 +46,21 @@ var app = new Vue({
             minutes = minutes < 10 ? `0${minutes}`: minutes
             seconds = seconds < 10 ? `0${seconds}`: seconds
             return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`
+        },
+        formatDate: function (dateString) {
+            var date = new Date(dateString)
+            var day = date.getDate(),
+                month = date.getMonth() + 1,
+                year = date.getFullYear()
+            day = day < 10 ? `0${day}`: day
+            month = month < 10 ? `0${month}`: month
+            return `${day}-${month}-${year}`
+        },
+        getTimestamp: function (dateString) {
+            return (new Date(dateString)).getTime()
+        },
+        isFirstGroup: function (groupName) {
+            return groupName === Object.keys(data.alertGroups)[0]
         }
     }
 })
